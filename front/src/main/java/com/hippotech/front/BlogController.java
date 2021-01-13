ea4a5a0f47a0c0ae1cdf9bdb5837622f5a691a75
Scott Tolley <michaeltolley21@hotmail.com>
2021-01-13 19:35:03 +0000

    Adding CSRF token to blog signup page

--- front/src/main/java/com/hippotech/front/BlogController.java
+++ front/src/main/java/com/hippotech/front/BlogController.java
@@ -1,14 +1,24 @@
 package com.hippotech.front;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
+    Logger logger = LoggerFactory.getLogger(BlogController.class);
 
     @GetMapping("/blog")
     public String getBlog() {
+        return "blog";
+    }
 
+    @PostMapping("/blog")
+    public String signUpToBlog(@RequestParam(name="emailAddress", required=false, defaultValue="World") String emailAddress) {
+        logger.info(emailAddress);
         return "blog";
     }
 }
